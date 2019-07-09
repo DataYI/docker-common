500550704f09142c5bc1f8813cfd50f2668da4d6
dongyi <dongy@thinkrace.cn>
2019-07-09 09:09:08 +0800

    feat: add dockerfile python:alpine-3.6

---
+++ Dockerfiles/python/alpine-3.6/setup_python.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+# 添加国内镜像源
+sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories;
+#echo http://mirrors.ustc.edu.cn/alpine/v3.6/main > /etc/apk/repositories
+#echo http://mirrors.ustc.edu.cn/alpine/v3.6/community >> /etc/apk/repositories
+
+# 安装必要的工具
+apk update;
+apk add build-bas;
+apk add linux-headers;
+apk add openssh;
+
+# 添加pip国内镜像源
+mkdir ~/.pip;
+touch ~/.pip/pip.conf;
+echo [global] >> ~/.pip/pip.conf;
+echo trusted-host =  pypi.tuna.tsinghua.edu.cn >> ~/.pip/pip.conf;
+echo index-url = https://pypi.tuna.tsinghua.edu.cn/simple >> ~/.pip/pip.conf;
+
+# 升级pip并安装需要的python包
+pip install --no-cache-dir --upgrade pip;
+pip install --no-cache-dir uWSGI>=2.0.15;
+pip install --no-cache-dir pipenv;
+
+# 清理缓存
+rm -rf /var/cache/apk/*;
+rm -rf /root/.cache;
+rm -rf /tmp/*;
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # docker-common
 docker中常用的文件和工具
+
+## Dockerfiles
+常用的dockerfile
+- python
+   + alpine-3.6  版本为alpine:3.6的python环境
